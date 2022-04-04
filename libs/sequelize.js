29789a0ec79c77f5c33ccec78856d54a79d0b369
Jesus Mejia <chucho4720013@gmail.com>
2022-04-04 17:42:10 -0500

    First Model in sequelize

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -1,6 +1,7 @@
 const { Sequelize } = require('sequelize');
 
 const { config } = require('../config/config');
+const setupModels = require('../db/models');
 
 const USER = encodeURIComponent(config.db.user);
 const PASSWORD = encodeURIComponent(config.db.password);
@@ -9,5 +10,9 @@ const URI = `postgres://${USER}:${PASSWORD}@${config.db.host}:${config.db.port}/
 const sequelize = new Sequelize(URI, {
 	dialect: 'postgres'
 });
+
+setupModels(sequelize);
+
+sequelize.sync();
 
 module.exports = sequelize;
