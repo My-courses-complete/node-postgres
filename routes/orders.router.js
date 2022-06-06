f8c6e2e1169b8e4fa8fa9abef5bec53629614305
Jesus Mejia <chucho4720013@gmail.com>
2022-06-06 09:58:44 -0500

    Ordenes de compra

--- routes/orders.router.js
+++ routes/orders.router.js
@@ -1,9 +1,69 @@
 const express = require('express');
 
-const router = express.Router();
+const OrderService = require('../services/order.service');
+const validatorHandler = require('../middlewares/validator.handler');
+const { getOrderSchema, createOrderSchema, updateOrderSchema } = require('../schemas/order.schema');
 
-router.get('/', (req, res) => {
-  res.json([]);
+const router = express.Router();
+const service = new OrderService();
+
+router.get('/', async (req, res, next) => {
+  try {
+    const orders = await service.find();
+    res.json(orders);
+  } catch (err) {
+    next(err);
+  }
 });
+
+router.get('/:id',
+  validatorHandler(getOrderSchema, 'params'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const order = await service.findOne(id);
+      res.json(order);
+    } catch (err) {
+      next(err);
+    }
+  });
+
+router.post('/',
+  validatorHandler(createOrderSchema, 'body'),
+  async (req, res, next) => {
+    try {
+      const body = req.body;
+      const newOrder = await service.create(body);
+      res.status(201).json(newOrder);
+    } catch (err) {
+      next(err);
+    }
+  });
+
+router.patch('/:id',
+  validatorHandler(getOrderSchema, 'params'),
+  validatorHandler(updateOrderSchema, 'body'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const body = req.body;
+      const order = await service.update(id, body);
+      res.json(order);
+    } catch (err) {
+      next(err);
+    }
+  });
+
+router.delete('/:id',
+  validatorHandler(getOrderSchema, 'params'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      await service.delete(id);
+      res.sendStatus(204);
+    } catch (err) {
+      next(err);
+    }
+  });
 
 module.exports = router;
--- services/order.service.js
+++ services/order.service.js
@@ -1,32 +1,48 @@
 const boom = require('@hapi/boom');
 
+const { models } = require('../libs/sequelize');
+
 class OrderService {
 
   constructor(){
   }
   async create(data) {
-    return data;
+    const newOrder = await models.Order.create(data);
+    return newOrder;
   }
 
   async find() {
-    return [];
+    const rta = await models.Order.findAll({
+      include: ['customer']
+    });
+    return rta;
   }
 
   async findOne(id) {
-    return { id };
+    const order = await models.Order.findByPk(id, {
+      include: [
+        {
+          association: 'customer',
+          include: ['user']
+        },
+      ]
+    });
+    if (!order) {
+      throw boom.notFound('order not found');
+    }
+    return order;
   }
 
   async update(id, changes) {
-    return {
-      id,
-      changes,
-    };
+    const order = await this.findOne(id);
+    const rta = order.update(changes);
+    return rta;
   }
 
   async delete(id) {
-    return { id };
+    const order = await this.findOne(id);
+    await order.destroy();
   }
-
 }
 
 module.exports = OrderService;
